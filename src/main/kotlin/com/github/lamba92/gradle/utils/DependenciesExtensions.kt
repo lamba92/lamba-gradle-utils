fbe2c448f0c73de2c5b94f545c31a84410288322
Lamberto Basti <basti.lamberto@gmail.com>
2020-04-15 14:38:20 +0200

    Fixed dependencies extensions types

--- src/main/kotlin/com/github/lamba92/gradle/utils/DependenciesExtensions.kt
+++ src/main/kotlin/com/github/lamba92/gradle/utils/DependenciesExtensions.kt
@@ -5,26 +5,26 @@ package com.github.lamba92.gradle.utils
 import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.jetbrains.kotlin.gradle.plugin.KotlinDependencyHandler
 
-fun KotlinDependencyHandler.ktor(module: String, version: String? = null): Any =
+fun KotlinDependencyHandler.ktor(module: String, version: String? = null) =
     "io.ktor:ktor-$module${version?.let { ":$version" } ?: ""}"
 
-fun KotlinDependencyHandler.lamba(module: String, version: String? = null): Any =
+fun KotlinDependencyHandler.lamba(module: String, version: String? = null) =
     "com.github.lamba92:$module${version?.let { ":$version" } ?: ""}"
 
-fun DependencyHandler.ktor(module: String, version: String? = null): Any =
+fun DependencyHandler.ktor(module: String, version: String? = null) =
     "io.ktor:ktor-$module${version?.let { ":$version" } ?: ""}"
 
-fun DependencyHandler.lamba(module: String, version: String? = null): Any =
+fun DependencyHandler.lamba(module: String, version: String? = null) =
     "com.github.lamba92:$module${version?.let { ":$version" } ?: ""}"
 
-fun KotlinDependencyHandler.kotlinx(module: String, version: String? = null, prefix: Boolean = true): Any =
+fun KotlinDependencyHandler.kotlinx(module: String, version: String? = null, prefix: Boolean = true) =
     "org.jetbrains.kotlinx:${if (prefix) "kotlinx-" else ""}$module${version?.let { ":$version" } ?: ""}"
 
-fun KotlinDependencyHandler.serialization(module: String, version: String? = null): Any =
+fun KotlinDependencyHandler.serialization(module: String, version: String? = null) =
     "org.jetbrains.kotlinx:kotlinx-serialization-$module${version?.let { ":$version" } ?: ""}"
 
-fun DependencyHandler.kotlinx(module: String, version: String? = null, prefix: Boolean = true): Any =
+fun DependencyHandler.kotlinx(module: String, version: String? = null, prefix: Boolean = true) =
     "org.jetbrains.kotlinx:${if (prefix) "kotlinx-" else ""}$module${version?.let { ":$version" } ?: ""}"
 
-fun DependencyHandler.serialization(module: String, version: String? = null): Any =
+fun DependencyHandler.serialization(module: String, version: String? = null) =
     "org.jetbrains.kotlinx:kotlinx-serialization-$module${version?.let { ":$version" } ?: ""}"
